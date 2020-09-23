906c93bdb8f7f47a1548183ce65cd864891f9d6b
FlutterRaj <rajkar921@gmail.com>
2020-09-24 04:48:49 +0530

    check reverse from an array for first repetative value

---
+++ nearest repetative value/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.Label1.Location = New System.Drawing.Point(444, 119)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 29)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Image = Global.nearest_repetative_value.My.Resources.Resources.gettyimages_1182636595_640x640
+        Me.Button3.ImageAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.Button3.Location = New System.Drawing.Point(420, 218)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(334, 51)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Display values"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.2!, System.Drawing.FontStyle.Bold)
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Image = Global.nearest_repetative_value.My.Resources.Resources.wp2234690_coding_wallpapers
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.Button2.Location = New System.Drawing.Point(403, 319)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(373, 67)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Nearest Repeated from Reverse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Imprint MT Shadow", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Image = Global.nearest_repetative_value.My.Resources.Resources.pngtree_color_abstract_gradient_blurred_halo_background_image_120001
+        Me.Button1.Location = New System.Drawing.Point(448, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(254, 43)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Enter values"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 31
+        Me.ListBox1.Location = New System.Drawing.Point(32, 23)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(324, 376)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.Label2.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.Label2.Location = New System.Drawing.Point(444, 148)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 29)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ nearest repetative value/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+    Dim arr(0) As Integer
+    Dim edge_case As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = Nothing
+        Label2.Text = Nothing
+        ListBox1.Items.Clear()
+
+        Dim size As Integer
+        Dim count As Integer = 0
+
+        edge_case = InputBox("How many value you want to add ? ")
+        If edge_case = "" Then
+            MessageBox.Show("Enter a valid integer")
+        ElseIf CInt(edge_case) < 1 Then
+            MessageBox.Show("Enter a positive integer")
+        Else
+            size = CInt(edge_case)
+            ReDim arr(size - 1)
+        End If
+
+        While count < size
+            edge_case = InputBox("Enter value " + Str(count + 1))
+            If edge_case = "" Then
+                MessageBox.Show("Enter a valid integer")
+            ElseIf CInt(edge_case) > 0 Then
+                arr(count) = CInt(edge_case)
+                count += 1
+            End If
+        End While
+        Label1.Text = CStr(size) + " values added !"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim start As Integer = 0
+        Dim last As Integer = arr.Length - 1
+        Dim count As Integer = 0
+        Dim sortest As New List(Of Integer)
+        Dim min As Integer = Integer.MaxValue
+
+        While start < last
+            If arr(start) = arr(last) Then
+                sortest.Add(count)
+            End If
+            count += 1
+            last -= 1
+
+            If start = last Then
+                start += 1
+                last = arr.Length - 1
+                count = 0
+            End If
+        End While
+
+        For Each element As Integer In sortest
+            min = Math.Min(min, element)
+        Next
+
+        If min < arr.Length Then
+            Array.Reverse(arr)
+            Label1.Text = "Nearest Repeated value"
+            Label2.Text = "from Reverse is " + CStr(arr(min))
+        Else
+            Label1.Text = "No Repeated value found "
+            ListBox1.Items.Add("             (˚Δ˚)b    ")
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = Nothing
+        Label2.Text = Nothing
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If arr.Length > 1 Then
+            For Each element As Integer In arr
+                ListBox1.Items.Add(element)
+            Next
+        Else
+            Label1.Text = "Add more than one value !! "
+            Label2.Text = "(≥o≤)"
+        End If
+    End Sub
+End Class
